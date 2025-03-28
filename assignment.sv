d42de9079f1cb6c765cd4193f0faa839e6aae42f
Hossein <h.beheshti92@gmail.com>
2025-03-28 17:36:26 +0000

    A52

--- assignment.sv
+++ assignment.sv
@@ -1,24 +1,22 @@
 `timescale 1ns / 1ps
 
-class unsigned_data;
-  int unsigned v1;
-  int unsigned v2;
-  int unsigned v3;
-endclass
-
-
 module tb;
 
-  unsigned_data c;
+
+  function void compare(int var1, int var2);
+    if (var1 == var2)
+    begin
+      $display("Test passed");
+    end
+    else
+    begin
+      $display("Test Failed");
+    end
+  endfunction
 
   initial
   begin
-    c = new();
-    c.v1 = 45;
-    c.v2 = 78;
-    c.v3 = 90;
-    #1;
-    $display("Value of v1: %0d, v2: %0d, and v3: %0d",c.v1, c.v2, c.v3);
+    compare(8,8);
   end
 
 endmodule
