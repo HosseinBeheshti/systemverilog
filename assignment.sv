b295a1b5a29d650bd4261802599bc70bef9ea4ed
Hossein <h.beheshti92@gmail.com>
2025-04-18 17:43:19 +0100

    A53

--- assignment.sv
+++ assignment.sv
@@ -1,22 +1,43 @@
 `timescale 1ns / 1ps
-
 module tb;
 
+  logic [5:0] addr;
+  logic wr, en;
 
-  function void compare(int var1, int var2);
-    if (var1 == var2)
-    begin
-      $display("Test passed");
-    end
-    else
-    begin
-      $display("Test Failed");
-    end
-  endfunction
+  bit clk = 0;
 
-  initial
-  begin
-    compare(8,8);
+  always #20 clk = ~clk;  // 40 ns --> 25 MHz
+
+  // Task to generate stimulus for addr, wr, and en
+  task generate_stimulus();
+    begin
+      @(posedge clk);
+      addr = 6'd12; wr = 1'b1; en = 1'b1;
+
+      @(posedge clk);
+      addr = 6'd14; wr = 1'b1; en = 1'b1;
+
+      @(posedge clk);
+      addr = 6'd23; wr = 1'b0; en = 1'b1;
+
+      @(posedge clk);
+      addr = 6'd48; wr = 1'b0; en = 1'b1;
+
+      @(posedge clk);
+      addr = 6'd56; wr = 1'b0; en = 1'b0;
+    end
+  endtask
+
+  initial begin
+    #10;
+    generate_stimulus();  // Call the stimulus generation task
+    #50;
+    $finish();
+  end
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
   end
 
 endmodule
